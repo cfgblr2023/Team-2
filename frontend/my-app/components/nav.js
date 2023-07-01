c0e4d73356d60a460220ad39cf310d9c78b4fc0c
ar7692@srmist.edu.in <AYUSH>
2023-07-01 17:36:51 +0530

    front

---
+++ frontend/my-app/components/nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function nav(){
+    return(
+        <div>
+            <ul>
+                <li>
+                    home
+                </li>
+            </ul>
+        </div>
+    )
+}
