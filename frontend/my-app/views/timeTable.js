03671342416e4c5c14a281d2e10203c40b46a8ee
naina-ag-25 <reach2nainaagarwal@gmail.com>
2023-07-02 08:27:35 +0530

    added mentor dashboard'

---
+++ frontend/my-app/views/timeTable.js
@@ -0,0 +1,133 @@
+const Week1 =[
+    {   
+        time: '4-5PM',
+        link: 'Sunday ',
+        subject: 'Extra',
+        // type: ''
+    }
+]
+const Week2 =[
+    {   
+        time: '09-10 AM',
+        link: '38-718',
+        subject: 'DBMS130',
+        // type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        link: '38-718',
+        subject: 'MTH166',
+        // type: 'Tutorial'
+    },
+    {   
+        time: '12-01 PM',
+        link: '38-718',
+        subject: 'NS200',
+        // type: 'Lecture'
+    }
+]
+const Week3 =[
+    {   
+        time: '09-10 AM',
+        link: '27-304Y',
+        subject: 'MTH166',
+        // type: 'Tutorial'
+    },
+    {   
+        time: '11-12 AM',
+        link: '28-107',
+        subject: 'CS849',
+        // type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        link: '28-107',
+        subject: 'CS849',
+        // type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        link: '38-718',
+        subject: 'NS200',
+        // type: 'Lecture'
+    }
+]
+
+const Week4 =[
+    {   
+        time: '10-11 AM',
+        link: '33-309',
+        subject: 'DBMS130',
+        // type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        link: '38-719',
+        subject: 'CS200',
+        // type: 'Lecture'
+    }
+]
+
+const Week5 =[
+    {   
+        time: '11-12 AM',
+        link: '33-309',
+        subject: 'MTH166',
+        // type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        link: '38-719',
+        subject: 'CS849',
+        // type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        link: '38-718',
+        subject: 'NS200',
+        // type: 'Lecture'
+    }
+]
+
+const Week6 =[
+    {   
+        time: '10-11 AM',
+        link: '33-309',
+        subject: 'MEC103',
+        // type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        link: '33-309',
+        subject: 'MEC103',
+        // type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        link: '33-601',
+        subject: 'CS849',
+        // type: 'Tutorial'
+    },
+
+]
+
+const Week7 =[
+    {   
+        time: '09-10 AM',
+        link: '34-604',
+        subject: 'DBMS130',
+        // type: 'Tutorial'
+    },
+    {   
+        time: '10-11 AM',
+        link: '34-604',
+        subject: 'DBMS130',
+        // type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '33-309',
+        subject: 'MTH166',
+        // type: 'Lecture'
+    }
+]
