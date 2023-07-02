4fb1337afe0c47100d273e7fde6f8bbee06c0557
anjali jha <anjalijha@anjalis-MacBook-Air.local>
2023-07-02 08:35:48 +0530

    added mentee-dashboard

---
+++ frontend/my-app/views/mentee-dashboard/timeTable.js
@@ -0,0 +1,62 @@
+const WEEK1 =[
+    {   
+        time: '09-10 AM(SUNDAY)',
+        MeetLink: 'https://meet.google.com/rgx-myvk-iep',
+        subject: 'No class Available',
+        
+    }
+]
+const WEEK2 =[
+    {   
+        time: '07-08 PM',
+        MeetLink: 'https://meet.google.com/ipu-jdiz-tiq',
+        subject: 'DBMS130',
+        
+    },
+
+]
+const WEEK3 =[
+    {   
+        time: '06-07 PM',
+        MeetLink: 'https://meet.google.com/rgx-myvk-iep',
+        subject: 'MTH166',
+        
+    },
+]
+
+const WEEK4 =[
+    {   
+        time: '10-11 AM',
+        MeetLink: 'https://meet.google.com/ipu-jdiz-tiq',
+        subject: 'DBMS130',
+       
+    },
+]
+
+const WEEK5 =[
+    {   
+        time: '11-12 AM',
+        MeetLink: 'https://meet.google.com/rgx-myvk-iep',
+        subject: 'MTH166',
+        
+    },
+]
+
+const WEEK6 =[
+    {   
+        time: '10-11 AM',
+        MeetLink: 'https://meet.google.com/ipu-jdiz-tiq',
+        subject: 'MEC103',
+       
+    },
+
+]
+
+const WEEK7 =[
+    {   
+        time: '09-10 AM',
+        MeetLink: 'https://meet.google.com/rgx-myvk-iep',
+        subject: 'DBMS130',
+        
+    },
+]
